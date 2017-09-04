eaf80cd2bab82101f632b5a7dd473f67f903a246
vmaletsky <volodymyr.maletsky@gmail.com>
2017-09-05 02:01:38 +0300

    SyncAdapter implementation

--- app/src/main/java/com/example/minihub/data/GithubProvider.java
+++ app/src/main/java/com/example/minihub/data/GithubProvider.java
@@ -15,7 +15,6 @@ import android.support.annotation.Nullable;
  */
 
 public class GithubProvider extends ContentProvider{
-    public static final String CONTENT_AUTHORITY = "com.example.minihub.data";
 
     private GithubDBHelper mDBHelper;
 
@@ -201,7 +200,7 @@ public class GithubProvider extends ContentProvider{
 
     static UriMatcher buildUriMatcher() {
         final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
-        final String authority = CONTENT_AUTHORITY;
+        final String authority = EventsContract.CONTENT_AUTHORITY;
 
         // For each type of URI you want to add, create a corresponding code.
         matcher.addURI(authority, UsersContract.PATH_USERS, USER);
--- app/src/main/java/com/example/minihub/sync/GithubSyncAdapter.java
+++ app/src/main/java/com/example/minihub/sync/GithubSyncAdapter.java
@@ -1,12 +1,16 @@
 package com.example.minihub.sync;
 
 import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.AbstractThreadedSyncAdapter;
 import android.content.ContentProviderClient;
+import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.SyncRequest;
 import android.content.SyncResult;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
@@ -35,6 +39,10 @@ import retrofit2.Response;
 public class GithubSyncAdapter extends AbstractThreadedSyncAdapter {
 
     private final String TAG = getClass().getSimpleName();
+
+    public static final int SYNC_INTERVAL = 60 * 180;
+    public static final int SYNC_FLEXTIME = SYNC_INTERVAL/3;
+    private static final long DAY_IN_MILLIS = 1000 * 60 * 60 * 24;
 
     public GithubSyncAdapter(Context context, boolean autoInitialize) {
         super(context, autoInitialize);
@@ -110,4 +118,89 @@ public class GithubSyncAdapter extends AbstractThreadedSyncAdapter {
         Log.d(TAG, "Sync complete. " + eventValuesVector.size() + " events inserted");
         Log.d(TAG, userValuesVector.size() + " users inserted");
     }
+
+    public static Account getSyncAccount(Context context) {
+        // Get an instance of the Android account manager
+        AccountManager accountManager =
+                (AccountManager) context.getSystemService(Context.ACCOUNT_SERVICE);
+
+        // Create the account type and default account
+        Account newAccount = new Account(
+                context.getString(R.string.app_name), context.getString(R.string.sync_account_type));
+
+        // If the password doesn't exist, the account doesn't exist
+        if ( null == accountManager.getPassword(newAccount) ) {
+
+        /*
+         * Add the account and account type, no password or user data
+         * If successful, return the Account object, otherwise report an error.
+         */
+            if (!accountManager.addAccountExplicitly(newAccount, "", null)) {
+                return null;
+            }
+            /*
+             * If you don't set android:syncable="true" in
+             * in your <provider> element in the manifest,
+             * then call ContentResolver.setIsSyncable(account, AUTHORITY, 1)
+             * here.
+             */
+
+            onAccountCreated(newAccount, context);
+        }
+        return newAccount;
+    }
+
+    private static void onAccountCreated(Account newAccount, Context context) {
+        /*
+         * Since we've created an account
+         */
+        GithubSyncAdapter.configurePeriodicSync(context, SYNC_INTERVAL, SYNC_FLEXTIME);
+
+        /*
+         * Without calling setSyncAutomatically, our periodic sync will not be enabled.
+         */
+        ContentResolver.setSyncAutomatically(newAccount, EventsContract.CONTENT_AUTHORITY, true);
+
+        /*
+         * Finally, let's do a sync to get things started
+         */
+        syncImmediately(context);
+    }
+
+    public static void initializeSyncAdapter(Context context) {
+        getSyncAccount(context);
+    }
+
+
+    /**
+     * Helper method to schedule the sync adapter periodic execution
+     */
+    public static void configurePeriodicSync(Context context, int syncInterval, int flexTime) {
+        Account account = getSyncAccount(context);
+        String authority = EventsContract.CONTENT_AUTHORITY;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // we can enable inexact timers in our periodic sync
+            SyncRequest request = new SyncRequest.Builder().
+                    syncPeriodic(syncInterval, flexTime).
+                    setSyncAdapter(account, authority).
+                    setExtras(new Bundle()).build();
+            ContentResolver.requestSync(request);
+        } else {
+            ContentResolver.addPeriodicSync(account,
+                    authority, new Bundle(), syncInterval);
+        }
+    }
+
+    /**
+     * Helper method to have the sync adapter sync immediately
+     * @param context The context used to access the account service
+     */
+    public static void syncImmediately(Context context) {
+        Bundle bundle = new Bundle();
+        bundle.putBoolean(ContentResolver.SYNC_EXTRAS_EXPEDITED, true);
+        bundle.putBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, true);
+        ContentResolver.requestSync(getSyncAccount(context),
+                EventsContract.CONTENT_AUTHORITY, bundle);
+    }
+
 }
